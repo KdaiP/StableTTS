6d8ddb63d9652b5c46a46dac44626c39419d543f
albluc24 <40117855+albluc24@users.noreply.github.com>
2024-08-26 10:08:47 +0200

    trying to fix issue #6 editing configs as recommended

--- vocos_pytorch/config.py
+++ vocos_pytorch/config.py
@@ -21,22 +21,32 @@ class MelConfig:
             self.pad = (self.n_fft - self.hop_length) // 2
             
 @dataclass
-class VocosConfig:
-    input_channels: int = 128
-    dim: int = 512
-    intermediate_dim: int = 1536
-    num_layers: int = 8
+class ModelConfig:
+    hidden_channels: int = 192
+    filter_channels: int = 512
+    n_heads: int = 2
+    n_enc_layers: int = 3 
+    n_dec_layers: int = 2 
+    kernel_size: int = 3
+    p_dropout: int = 0.1
+    gin_channels: int = 192
             
 @dataclass
 class TrainConfig:
-    train_dataset_path: str = './filelists/filelist.txt'
-    test_dataset_path: str = './filelists/filelist.txt'
-    batch_size: int = 22
+    train_dataset_path: str = 'filelists/filelist.json'
+    test_dataset_path: str = 'filelists/filelist.json'
+    batch_size: int = 48
     learning_rate: float = 1e-4
     num_epochs: int = 10000
     model_save_path: str = './checkpoints'
     log_dir: str = './runs'
     log_interval: int = 128
+    save_interval: int = 15
     warmup_steps: int = 200
     
-    segment_size = 20480
+@dataclass
+class VocosConfig:
+    input_channels: int = 128
+    dim: int = 512
+    intermediate_dim: int = 1536
+    num_layers: int = 8
